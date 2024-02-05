773b350d9145709db4186cca8cd222c627829cab
whatcodehaveyougit <sigurdwatt@gmail.com>
2024-02-05 17:01:07 +0000

    Add events

--- helloWorld/script.js
+++ helloWorld/script.js
@@ -2,7 +2,26 @@ console.log('helo')
 
 const app = Vue.createApp({
   data() {
-    return {title: 'Superman' }
+    return {
+      title: 'Superman',
+      age: 22,
+      showInfo: true
+    }
+  },
+  methods: {
+    changeTitle(params){
+      this.title = params
+    },
+    showOrHideInfo(){
+      this.showInfo = !this.showInfo
+    },
+    onMouseOver(){
+      console.log('mouse over')
+    },
+    handleEvent(e){
+      console.log(e)
+    }
+
   }
 })
 
