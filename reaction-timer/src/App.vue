bc9c5198aa99e252d8e08ff31aa4955bc881fd06
whatcodehaveyougit <sigurdwatt@gmail.com>
2024-02-13 17:38:26 +0000

    Complete reaction timer game

---
+++ reaction-timer/src/App.vue
@@ -0,0 +1,73 @@
+<template>
+<div>
+  <h3>Ninja Reaction Timer</h3>
+  <div v-if="showBlock">
+    <Block  @stopClick="stopTimer" />
+  </div>
+  <div v-if="!showBlock">
+    <button @click="startGame">Play</button>
+  </div>
+  <div v-if="timeTakenToClick">
+    User Played Game
+    {{ timeTakenToClick }} milliseconds
+    <h5>{{ userMessage }}</h5>
+  </div>
+</div>
+</template>
+
+<script>
+import Block from './components/Block.vue'
+
+function randomIntFromInterval(min, max) { // min and max included
+  const number = Math.floor(Math.random() * (max - min + 1) + min)
+  return number * 1000
+}
+
+function generateMessage(timeTaken){
+  if (timeTaken > 500){
+    return "Too slow try again!"
+  } else {
+    return "Super quick, well done!"
+  }
+}
+
+export default {
+  name: 'App',
+  components: {
+    Block
+  },
+   data() {
+    return {
+      showBlock: false,
+      startTimeClick: 0,
+      timeTakenToClick: 0,
+      userMessage: ''
+    }
+  },
+  methods:{
+    startGame(){
+      const rndInt = randomIntFromInterval(1, 3)
+      setTimeout( () => {
+        this.showBlock = true;
+        this.startTimeClick = Date.now()
+      }, rndInt)
+    },
+    stopTimer(){
+      const res = Date.now() - this.startTimeClick
+      this.timeTakenToClick = res;
+      this.userMessage = generateMessage(res)
+    },
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ reaction-timer/src/components/Block.vue
@@ -0,0 +1,24 @@
+<template>
+<div id="block" @click="stopClick"></div>
+</template>
+
+<script>
+export default {
+  name: 'Block',
+  methods: {
+    stopClick() {
+        this.$emit('stopClick')
+      }
+  }
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+#block {
+  width: 300px;
+  height: 300px;
+  background-color: green;
+}
+</style>
