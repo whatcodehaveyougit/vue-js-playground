28a1542ff7a87f90a631e998d55c528612ed0d0a
whatcodehaveyougit <sigurdwatt@gmail.com>
2024-02-14 14:33:26 +0000

    Write comment detailing what is wrong with unit tst

--- reaction-timer/tests/unit/app.spec.js
+++ reaction-timer/tests/unit/app.spec.js
@@ -5,6 +5,8 @@ import { nextTick } from 'vue'
 describe('App.vue', () => {
   it('user can press button', async () => {
     const wrapper = shallowMount(App)
+    // I want to test the logic here in this App components which shows or hides the child components
+    // Not working for a number of reasons
     wrapper.find('button').trigger('click')
     await nextTick();
     expect(wrapper.html()).toContain('Click Me to Stop Game')
